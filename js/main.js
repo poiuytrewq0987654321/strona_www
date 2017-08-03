941356ba9498a226cb14c3cf4b203b79d9800560
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-08-03 11:32:38 +0200

    commit
    dodane nowe zdjecia, dodany sliderTop dzialajacy na wszystkich
    przegladarkach

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();    
-    if (scroll >= 50) {
-        $("#main-nav").css('background-color','#0ed8d6').css('opacity','1')
-    }else{
-        $("#main-nav").css('background-color','transparent').css('opacity','1')
-    }
-});
+'use strict';
 
 $("a[href*='#']").click(function(){
-    $('body').animate({
+    $('html, body').animate({
         scrollTop: $(this.hash).offset().top - 10
     },500);
 })
