dc56f9621624d67a68326351c758c001c8134693
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-08-03 15:56:54 +0200

    coomit
    wstepne zabawy z AJAX i pobraniem danych ze zdalnego serwera

--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ $("a[href*='#']").click(function(){
         scrollTop: $(this.hash).offset().top - 10
     },500);
 })
+
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
+$.get(
+    'http://www.radiozet.pl/Radio/Lista-przebojow',
+    function (response) {
+        console.log("> ", response);
+        $("#viewer").html(response);
+});
