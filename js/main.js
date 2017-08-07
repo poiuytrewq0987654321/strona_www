a1f42627065e4347bdb0f4d191e4383c59aa5449
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-08-07 15:04:11 +0200

    commit
    pierwsze zabawy z chart.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 'use strict';
 
-$("a[href*='#']").click(function(){
+$("a[href*='#']").click(function () {
     $('html, body').animate({
         scrollTop: $(this.hash).offset().top - 10
-    },500);
-})
-
-$.ajaxPrefilter( function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
-    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    //options.url = "http://cors.corsproxy.io/url=" + options.url;
-  }
+    }, 500);
 });
 
-$.ajaxPrefilter( function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
-    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    //options.url = "http://cors.corsproxy.io/url=" + options.url;
-  }
+$.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+        options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+        //options.url = "http://cors.corsproxy.io/url=" + options.url;
+    }
+});
+
+$.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+        options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+        //options.url = "http://cors.corsproxy.io/url=" + options.url;
+    }
 });
 
 $.get(
@@ -27,4 +27,9 @@ $.get(
     function (response) {
         console.log("> ", response);
         $("#viewer").html(response);
-});
+    });
+
+
+
+
+
